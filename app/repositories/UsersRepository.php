a9521832c774b953137abb1e35d2d6c544187fb9
Fellipe Esteves Pereira <fellipeep2005@gmail.com>
2024-04-09 22:10:12 -0300

    refactor: create user function

--- app/repositories/UsersRepository.php
+++ app/repositories/UsersRepository.php
@@ -16,28 +16,37 @@ class UsersRepository
         $this->database = $database;
     }
 
-    public function createUser($nome, $data_nasc, $sexo, $nome_mat, $cpf, $email, $celular, $telefone, $cep, $estado, $cidade, $rua, $logradouro, $login, $senha)
+    public function listarcolunas(): array
     {
-        $sql = 'INSERT INTO users (nome, data_nasc, sexo, nome_mat, cpf, email, celular, telefone, cep, estado, cidade, rua, logradouro, login, senha) values (:nome, :data_nasc, :sexo, :nome_mat, :cpf, :email, :celular, :telefone, :cep, :estado, :cidade, :rua, :logradouro, :login, :senha)';
+        $pdo = $this->database->getConnection();
+
+        $stmt = $pdo->query('SHOW COLUMNS FROM users');
+
+        return $stmt->fetchAll(PDO::FETCH_COLUMN);
+    }
+
+    public function createUser(array $columns)
+    {
+        $sql = 'INSERT INTO users (nome, datanasc, sexo, nome_mat, cpf, email, celular, telefone, cep, estado, cidade, rua, logradouro, login, senha) values (:nome, :datanasc, :sexo, :nome_mat, :cpf, :email, :celular, :telefone, :cep, :estado, :cidade, :rua, :logradouro, :login, :senha)';
 
         $pdo = $this->database->getConnection();
 
         $stmt = $pdo->prepare($sql);
-        $stmt->bindValue(':nome', $nome);
-        $stmt->bindValue(':data_nasc', $data_nasc);
-        $stmt->bindValue(':sexo', $sexo);
-        $stmt->bindValue(':nome_mat', $nome_mat);
-        $stmt->bindValue(':cpf', $cpf);
-        $stmt->bindValue(':email', $email);
-        $stmt->bindValue(':celular', $celular);
-        $stmt->bindValue(':telefone', $telefone);
-        $stmt->bindValue(':cep', $cep);
-        $stmt->bindValue(':estado', $estado);
-        $stmt->bindValue(':cidade', $cidade);
-        $stmt->bindValue(':rua', $rua);
-        $stmt->bindValue(':logradouro', $logradouro);
-        $stmt->bindValue(':login', $login);
-        $stmt->bindValue(':senha', $senha);
+        $stmt->bindValue(':nome', $columns[1]);
+        $stmt->bindValue(':datanasc', $columns[2]);
+        $stmt->bindValue(':sexo', $columns[3]);
+        $stmt->bindValue(':nome_mat', $columns[4]);
+        $stmt->bindValue(':cpf', $columns[5]);
+        $stmt->bindValue(':email', $columns[6]);
+        $stmt->bindValue(':celular', $columns[7]);
+        $stmt->bindValue(':telefone', $columns[8]);
+        $stmt->bindValue(':login', $columns[9]);
+        $stmt->bindValue(':senha', $columns[10]);
+        $stmt->bindValue(':cep', $columns[11]);
+        $stmt->bindValue(':estado', $columns[12]);
+        $stmt->bindValue(':cidade', $columns[13]);
+        $stmt->bindValue(':rua', $columns[14]);
+        $stmt->bindValue(':logradouro', $columns[15]);
         return $stmt->execute();
     }
 }
