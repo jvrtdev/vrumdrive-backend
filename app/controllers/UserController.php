88a6fa03f5ecc2b3ead6191eb4556122ac99efeb
Fellipe Esteves Pereira <fellipeep2005@gmail.com>
2024-04-12 15:10:31 -0300

    feat: update createUser, add function cpf verificator

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -20,7 +20,43 @@ class UserController
   {
       $columns = $this->userRepository->getColumns();
 
-      $data = $request->getParsedBody();
+      $data = get_object_vars(json_decode($request->getBody()));
+
+      //Verificação do cpf início --------------------------------------
+      $cpf = preg_replace('/[^0-9]/', '', $data["cpf"]);
+
+      if (strlen($cpf) != 11) 
+      {
+          $response->getBody()->write(json_encode(['message' => 'Failed to create user']));
+          return $response->withStatus(400)->withHeader('Content-Type', 'application/json');
+      }
+
+      if (preg_match('/(\d)\1{10}/', $cpf)) 
+      {
+          $response->getBody()->write(json_encode(['message' => 'Failed to create user']));
+          return $response->withStatus(400)->withHeader('Content-Type', 'application/json');
+      }
+      
+      for ($i = 10, $soma = 0, $k = 0; $i > 1; $i--) 
+      {
+          $soma += $cpf[$k++] * $i;
+      }
+      
+      $verificador1 = (($soma % 11) < 2) ? 0 : 11 - ($soma % 11);
+      
+      for ($i = 11, $soma = 0, $k = 0; $i > 1; $i--) 
+      {
+          $soma += $cpf[$k++] * $i;
+      }
+      
+      $verificador2 = (($soma % 11) < 2) ? 0 : 11 - ($soma % 11);
+
+      if ($verificador1 != $cpf[9] || $verificador2 != $cpf[10]) 
+      {
+          $response->getBody()->write(json_encode(['message' => 'Failed to create user']));
+          return $response->withStatus(400)->withHeader('Content-Type', 'application/json');
+      }
+      //Verificação do cpf fim --------------------------------------
 
       for($i = 1; $i < count($columns); $i++){
         $data_columns[$columns[$i]] = $data[$columns[$i]];
