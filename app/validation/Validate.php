054ecb23181c74f6a320e55332df95d679f1ef7c
Fellipe Esteves Pereira <fellipeep2005@gmail.com>
2024-04-12 17:46:07 -0300

    create validate.php

---
+++ app/validation/Validate.php
@@ -0,0 +1,44 @@
+<?php
+namespace App\Validation;
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+
+class Validate
+{
+    public function cpfValidator($cpf)
+    {
+      $cpf = preg_replace('/[^0-9]/', '', $cpf);
+
+      if (strlen($cpf) != 11) 
+      {
+          return "Número de caractéres inválido";
+      }
+
+      if (preg_match('/(\d)\1{10}/', $cpf)) 
+      {
+          return "CPF inválido";
+      }
+      
+      for ($i = 10, $soma = 0, $k = 0; $i > 1; $i--) 
+      {
+          $soma += $cpf[$k++] * $i;
+      }
+      
+      $verificador1 = (($soma % 11) < 2) ? 0 : 11 - ($soma % 11);
+      
+      for ($i = 11, $soma = 0, $k = 0; $i > 1; $i--) 
+      {
+          $soma += $cpf[$k++] * $i;
+      }
+      
+      $verificador2 = (($soma % 11) < 2) ? 0 : 11 - ($soma % 11);
+
+      if ($verificador1 != $cpf[9] || $verificador2 != $cpf[10]) 
+      {
+        return "CPF inexistente";
+      }
+
+      return false;
+    }
+}
