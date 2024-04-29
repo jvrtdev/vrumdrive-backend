06e5ecbaa98be4deef2fec1364f7ceaa8d0f6718
jvrtdevmac <joaovitorromanoteixeira@gmail.com>
2024-04-29 12:43:37 -0300

    :sparkles: feat: add authToken algorithm and add exp time token

--- app/validation/AuthUser.php
+++ app/validation/AuthUser.php
@@ -1,46 +1,80 @@
 <?php
 namespace App\Validation;
 
+use App\Database;
+use App\Repositories\UsersRepository;
 use Exception;
 use \Firebase\JWT\JWT;
+use Firebase\JWT\Key;
 
 
 class AuthUser
 {
-  private $secret_key;
+  protected $secret_key;
 
+  protected $userRepository;
+
+  protected int $time;
+  
   public function __construct($secret_key)
   {
     $this->secret_key = $secret_key;
+    
+    $database = new Database;
+      
+    $this->userRepository = new UsersRepository($database);
+
+    $this->time = 30;//tempo em segundos de expiracao do token
+
   }
   
   
-  public function createToken($userData)
+  public function createToken($userData) :string
   {
+    $now = time();
+    $expirationTime = $now + $this->time;
+
     $payload = [
       'cpf' => $userData['cpf'],
       'login' => $userData['login'],
       'senha' => $userData['senha'],
+      'exp' => $expirationTime,
     ];
     
     $jwt = JWT::encode($payload, $this->secret_key , 'HS256');//args->informacoes, chave secreta, criptografia
     
     return $jwt;
   }
+  
+  
 
-
-  public function authToken($token)
+  public function authToken($token) : bool
   {
-    try {
-      $decoded = JWT::decode($token, $this->secret_key);
-      
-      $userCpf = $decoded->cpf;
-      //falta adicionar a logica aqui para fazer a autenticação do token com as informacoes do usuario
-      return $userCpf;
-      
-    } catch (Exception $e) {
-      # code...
-      echo $e->getMessage();
+    if ($token) {
+      // Verifica se a string começa com "Bearer "
+      if (strpos($token, 'Bearer ') === 0) {
+          // Remove o prefixo "Bearer " da string
+          $token = substr($token, 7);
+      }
+      try {
+        $decoded = JWT::decode($token, new Key($this->secret_key, 'HS256'));
+        
+        $userCpfToken = $decoded->cpf;
+        $data = $this->userRepository->getAuth($userCpfToken);
+        $userCpfFromDb = $data[0]->cpf;
+        
+
+         if($userCpfToken == $userCpfFromDb ){
+           return true;
+         }
+       } catch (Exception $e) {
+         print_r($e->getMessage());
+         return false;
+       }
+    }
+    else{
+      print_r("O token inexistente");
+      return false;
     }
   }
 }
