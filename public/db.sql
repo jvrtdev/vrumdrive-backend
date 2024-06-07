421a7c944ff0e1cbb2faeecc8d97dbfd2c9f1375
jvrtdev-linux <joaovitorromanoteixeira@gmail.com>
2024-06-07 10:14:34 -0300

    refactor: alterando modelacao do banco de dados

--- public/db.sql
+++ public/db.sql
@@ -1,42 +1,53 @@
 CREATE TABLE users (
-  id_user INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   id_address INT NOT NULL,
-  name VARCHAR(50) NOT NULL,
-  birth_date DATE NOT NULL,
-  gender ENUM('F','M','O') NOT NULL,
-  maternal_name VARCHAR(50) NOT NULL,
+  nome VARCHAR(50) NOT NULL,
+  data_nasc DATE NOT NULL,
+  genero ENUM('F','M','O') NOT NULL,
+  nome_mat VARCHAR(50) NOT NULL,
   cpf VARCHAR(15) NOT NULL,
   email VARCHAR(80) NOT NULL,
-  cellphone VARCHAR(17) NOT NULL,
+  celular VARCHAR(17) NOT NULL,
   login VARCHAR(6) NOT NULL,
   senha VARCHAR(255) NOT NULL,
 
-  FOREIGN KEY(id_address) REFERENCES address(id)
+  FOREIGN KEY(id_address) REFERENCES address(id_address)
 );
 
-CREATE TABLE cars (
-  id_car INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE vehicles (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   img VARCHAR(30) NOT NULL,
-  model VARCHAR(30) NOT NULL,
-  brand VARCHAR(30) NOT NULL,
-  category ENUM('SUV', 'Sedan', 'Hatchback', 'Pick-up', 'Sport', 'Luxury', 'Sports car'),
-  year VARCHAR(4) NOT NULL,
-  status ENUM('spare', 'busy') NOT NULL,
-  price FLOAT NOT NULL
+  modelo VARCHAR(30) NOT NULL,
+  marca VARCHAR(30) NOT NULL,
+  categoria ENUM('SUV', 'Sedan', 'Hatchback', 'Pick-up', 'Esportivo', 'Luxo', 'Compacto'),
+  ano VARCHAR(4) NOT NULL,
+  status ENUM('disponivel', 'ocupado') NOT NULL,
+  preco FLOAT NOT NULL
+);
+
+CREATE TABLE vehicles_details (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  id_vehicle INT NOT NULL,
+  quilometragem VARCHAR NOT NULL,
+  motor VARCHAR(30) NOT NULL,
+  cor VARCHAR(30) NOT NULL,
+  preco FLOAT NOT NULL,
+  detalhes TEXT NOT NULL,
+
+  FOREIGN KEY(id_vehicle) REFERENCES vehicles(id)
+
 );
 
 CREATE TABLE bookings (
-  id_booking INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  id_user INT NOT NULL,
-  id_car INT NOT NULL,
-  start_date DATE NOT NULL,
-  end_date DATE NOT NULL,
-  start_time TIME NOT NULL,
-  end_time TIME NOT NULL,
-  total_price FLOAT NOT NULL
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  user_id INT NOT NULL,
+  vehicle_id INT NOT NULL,
+  pickup_date_time DATETIME NOT NULL,
+  return_date_time DATETIME NOT NULL,
+  total_price FLOAT NOT NULL,
 
-  FOREIGN KEY(id_user) REFERENCES users(id),
-  FOREIGN KEY(id_car) REFERENCES cars(id)
+  FOREIGN KEY(id_user) REFERENCES users(id_user),
+  FOREIGN KEY(id_car) REFERENCES cars(id_car)
 );
 
 CREATE TABLE address (
