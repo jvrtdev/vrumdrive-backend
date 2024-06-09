1cdecc7ce3dc9517e7c7e80c8ee2da2141dcf1d3
jvrtdev-linux <joaovitorromanoteixeira@gmail.com>
2024-06-08 23:59:18 -0300

    fix: corrigindo namespace e adicionando metodo de calculo de preco

--- app/routes/bookings.php
+++ app/routes/bookings.php
@@ -1,14 +1,15 @@
 <?php
 
+use App\Controllers\BookingController;
 use Slim\Routing\RouteCollectorProxy;
 use App\Controllers\UserController;
-use Middleware\JwtMiddleware;
+use App\Middleware\AuthJwt;
 
 return function ($app) {
     // Rotas sem JWT
-    
+    $app->post("/api/booking/price", BookingController::class . ':getTotalPriceBooking');
     // Rotas protegidas com JWT
     $app->group('/api', function (RouteCollectorProxy $group) {
-       
-    })->add(new JwtMiddleware());
+       $group->post("/api/booking/create", BookingController::class . ':createNewBooking');
+    })->add(new AuthJwt());
 };
