96e52ee186a3f7982448461880d6f52e76141ea5
Nilozs <144507366+Nilozs@users.noreply.github.com>
2024-06-12 17:33:39 -0300

    fix: Tornando a rota de log privada

--- app/routes/log.php
+++ app/routes/log.php
@@ -5,5 +5,7 @@ use App\Controllers\LogController;
 use App\Middleware\AuthJwt;
 
 return function ($app) {
-    $app->get('/api/log', LogController::class . ':logUser');
+    $app->group('/api', function (RouteCollectorProxy $group) {
+        $group->get('/log', LogController::class . ':logUser');
+      })->add(new AuthJwt());
 };
