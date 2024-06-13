e022d09393c7fe2696b63ef57be387c8dbd9200e
Fellipe Esteves Pereira <fellipeep2005@gmail.com>
2024-06-12 22:41:51 -0300

    remove: deleção das rotas create e update do address

--- app/routes/users.php
+++ app/routes/users.php
@@ -14,8 +14,6 @@ return function ($app) {
     $app->group('/api', function (RouteCollectorProxy $group) {
         $group->get('/user/{id}', UserController::class . ':getUserById');
         $group->get('/users', UserController::class . ':getUsers');
-        $group->post('/address/create/{id}', UserController::class . ':createAddress');
-        $group->put('/address/update/{id}', UserController::class . ':updateAddress');
         $group->put('/user/update/{id}', UserController::class . ':updateUser');
         $group->delete('/user/delete/{id}', UserController::class . ':deleteUser');
     })->add(new AuthJwt());
