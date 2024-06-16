221da3a0381c36ec57e2ff00de60b5996c647900
jvrtdev-linux <joaovitorromanoteixeira@gmail.com>
2024-06-16 17:08:01 -0300

    nao aguento mais esse projeto irmao

--- public/index.php
+++ public/index.php
@@ -21,7 +21,7 @@ $app->options('/{routes:.+}', function ($request, $response, $args) {
 $app->add(function ($request, $handler) {
   $response = $handler->handle($request);
   return $response
-    ->withHeader('Access-Control-Allow-Origin', 'http://localhost:3000')
+    ->withHeader('Access-Control-Allow-Origin', '*')
     ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
     ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
 });
