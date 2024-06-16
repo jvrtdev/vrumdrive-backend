243e0d477e6bb2fd9c53e67c359d3e2223a58663
jvrtdev-linux <joaovitorromanoteixeira@gmail.com>
2024-06-16 14:50:02 -0300

    refacotr: add auth admin in log routes

--- app/routes/log.php
+++ app/routes/log.php
@@ -2,13 +2,13 @@
 
 use Slim\Routing\RouteCollectorProxy;
 use App\Controllers\LogController;
-use App\Middleware\AuthJwt;
+use App\Middleware\AuthAdminJwt;
 
 return function ($app) {
     $app->group('/api', function (RouteCollectorProxy $group) {
-      $group->get('/log', LogController::class . ':getAllLogs');
-      $group->get('/log/filter/{value}', LogController::class . ':getFilterLogs');
-      $group->get('/log/{id}', LogController::class . ':getLogsById');
-      $group->post('/log/create', LogController::class . ':createLog');
-    })->add(new AuthJwt());
+      $group->get('/admin/log', LogController::class . ':getAllLogs');
+      $group->get('/admin/log/filter/{value}', LogController::class . ':getFilterLogs');
+      $group->get('/admin/log/{id}', LogController::class . ':getLogsById');
+      $group->post('/admin/log/create', LogController::class . ':createLog');
+    })->add(new AuthAdminJwt());
 };
