757f2b9014b6534a427d06f8a0240a951077bb52
jvrtdev-linux <joaovitorromanoteixeira@gmail.com>
2024-06-19 01:41:41 -0300

    nao deu certo o retorno vai ter que ser img para cada imagem

--- app/controllers/VehicleController.php
+++ app/controllers/VehicleController.php
@@ -43,7 +43,7 @@ class VehicleController
         
         $imgs = $this->vehicleRepository->getImagesByVehicleId($result['id_vehicle']);
 
-        $response->getBody()->write(json_encode($imgs[0]));
+        $response->getBody()->write(json_encode($imgs));
       
         return $response->withStatus(200)->withHeader('Content-Type', 'application/json');
      }
