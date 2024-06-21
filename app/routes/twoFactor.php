6862d9f7e6b108dd52316009d381a83418894f29
Fellipe Esteves Pereira <fellipeep2005@gmail.com>
2024-06-21 11:36:08 -0300

    feat: iniciando uma sessão

--- app/routes/twoFactor.php
+++ app/routes/twoFactor.php
@@ -4,6 +4,8 @@ use Slim\Routing\RouteCollectorProxy;
 use App\Controllers\twoFactorController;
 use App\Middleware\AuthJwt;
 
+session_start();
+
 return function ($app) {
     // Rotas protegidas com JWT
     $app->group('/api', function (RouteCollectorProxy $group) {
